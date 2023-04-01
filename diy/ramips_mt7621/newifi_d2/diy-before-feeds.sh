93d36cba394cad28fb0154208fb3617f9a4c367f
Langdon Lin <me@langd.one>
2023-04-01 23:03:54 +0800

    remove: non-related file from the repository

--- diy/ramips_mt7621/newifi_d2/diy-before-feeds.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Use master branch of helloworld
-git clone --depth=1 https://github.com/fw876/helloworld package/helloworld
-
-# Use master branch of passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/passwall
-
