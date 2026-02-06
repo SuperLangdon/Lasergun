f5889edfdc533e7a36c2a44766e87fb4a5685c84
Langdon Lin <me@langd.one>
2026-02-06 22:51:43 +0800

    Add mosdns and enable Chinese i18n packages
    Change diy-before-feeds to clone mosdns directly from sbwml (branch v5) into package/mosdns and remove previous theme/design clones. Keep turboacc addition unchanged. Update seeds: enable Simplified Chinese (zh_Hans), add autocore and default-settings (including CHN variant), enable luci-theme-argon and multiple luci-i18n-*-zh-cn packages, enable luci-app-mosdns and wget-ssl, and remove several legacy filesystem/storage and lua5.3 package entries. These changes switch mosdns sourcing to a direct repo and broaden Chinese language support in the image.

--- diy/ipq40xx/diy-before-feeds.sh
+++ diy/ipq40xx/diy-before-feeds.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Add mosdns
-echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns;v5' >> feeds.conf.default
-# Add design theme and config plugin
-git clone https://github.com/0x676e67/luci-theme-design.git package/luci-theme-design
-git clone https://github.com/0x676e67/luci-app-design-config.git package/luci-app-design-config
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 # Add turboacc without nft-fullcone
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 rm -rf package/turboacc/nft-fullcone
