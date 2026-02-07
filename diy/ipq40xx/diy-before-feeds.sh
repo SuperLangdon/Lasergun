dc6f87bb5fb1ac52be54700c4c84fa745bb18a66
Langdon Lin <me@langd.one>
2026-02-07 12:15:22 +0800

    Remove existing mosdns Makefiles before clone
    Delete any existing mosdns Makefile files to avoid duplicate/conflicting package entries, then clone the sbwml luci-app-mosdns repo into package/mosdns. Keeps the existing turboacc setup and removal of nft-fullcone.

--- diy/ipq40xx/diy-before-feeds.sh
+++ diy/ipq40xx/diy-before-feeds.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Add mosdns
+find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+
 # Add turboacc without nft-fullcone
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 rm -rf package/turboacc/nft-fullcone
